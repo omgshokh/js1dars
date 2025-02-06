87e1254e0a7289adb91b0c3b8ea0f791385c678b
omgshokh <shohruz199905@gmail.com>
2025-02-06 15:58:31 +0500

    Add files via upload

---
+++ svript.js
@@ -0,0 +1,23 @@
+
+
+let ism = prompt('Ismingizni kiriting');
+alert(ism)
+console.log(ism)
+   
+let familya = prompt( 'Familyangizni kiriting')
+alert(familya)
+console.log(familya);
+
+
+let yosh =prompt(' yoshingizni kiriting')
+alert(yosh)
+console.log(yosh);
+
+let vazifa= prompt('  Yoqtirgan vazifangizni kiriting,')
+alert(vazifa)
+console.log(vazifa);
+
+
+let tuman = prompt('Yashaydiga tumaningizni kiriting')
+alert(tuman)
+console.log(tuman);
